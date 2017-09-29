dc1db9f0d40e132f0361322a875d44581f32240b
AJ <aj.nice@kakaocorp.com>
2017-09-29 09:44:35 +0900

    일단 div동적으로 필요한 16개 만들어보기

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 console.log('chess');
 
 
-
+var wrap = document.querySelector('.wrap');
+var str = '';
 
 // 마크업을 코드로 찍어주기
+
+for(var i=0; i < 16; i++){
+
+  str += '<div class="black"></div>';
+}
+
+wrap.innerHTML = str;
 
 
 // DOM에 클릭 이벤트 할당
