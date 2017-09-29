5011b63a0d31749a263df128503a0ac96e84bba9
AJ <aj.nice@kakaocorp.com>
2017-09-29 10:22:08 +0900

    백,흑 번갈아가면서 bg컬러 주도록 .

--- app.js
+++ app.js
@@ -3,12 +3,26 @@ console.log('chess');
 
 var wrap = document.querySelector('.wrap');
 var str = '';
+var className = '';
+var isWhite = false;
+
 
 // 마크업을 코드로 찍어주기
-
 for(var i=0; i < 16; i++){
 
-  str += '<div class="black"></div>';
+  if(isWhite) {
+    className = 'white';
+  }
+  else {
+    className = 'black';
+  }
+
+  str += '<div class="'+ className +'"></div>';
+
+
+  //한번씩 변경
+  isWhite = !isWhite;
+
 }
 
 wrap.innerHTML = str;
