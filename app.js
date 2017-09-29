85ef069e528e31106758b6e56b5acb64000a99c3
AJ <aj.nice@kakaocorp.com>
2017-09-29 13:27:42 +0900

    for 루프 두번으로 변경

--- app.js
+++ app.js
@@ -8,25 +8,24 @@ var isWhite = false;
 
 
 // 마크업을 코드로 찍어주기
-for(var i=0; i < 16; i++){
+for(var i=0; i < 4; i++){
 
-  if(isWhite) {
-    className = 'white';
-  }
-  else {
-    className = 'black';
-  }
+  for(var j=0; j < 4; j++){
 
-  str += '<div class="'+ className +'"></div>';
+    if(isWhite) {
+      className = 'white';
+    }
+    else {
+      className = 'black';
+    }
 
+    str += '<div class="'+ className +'"></div>';
 
-  //한번씩 변경
-  isWhite = !isWhite;
-
-  if(i % 4 === 3 ){
+    //한번씩 변경
     isWhite = !isWhite;
   }
 
+  isWhite = !isWhite;
 }
 
 wrap.innerHTML = str;
